8c08cdf336ed4236f6ad7c26f9d92f11801baeaa
rinat <mambetovpost@gmail.com>
2023-09-16 08:02:39 +0300

    simple func and class components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
+  // 1
+  // let likes = 0;
+  // function increment() {
+  //   likes++;
+  //   console.log("likes", likes);
+  // }
+
+  // 2
+  // const state = useState(5);
+
+  // 3
+  // const [count, setCount] = useState(5);
+  // console.log("count", count);
+  // console.log("setCount", setCount);
+
+  // 4
+  // const [likes, setLikes] = useState(0);
+
+  // function increment() {
+  //   setLikes(likes + 1);
+  // }
+
+  // function decrement() {
+  //   setLikes(likes - 1);
+  // }
+
+  const [value, setValue] = useState("Text in Input");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
+      <ClassCounter />
     </div>
   );
 }
