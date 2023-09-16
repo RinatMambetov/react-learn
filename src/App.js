c4d83a30dd499340482ec7829b4d93b3e0c5f6aa
rinat <mambetovpost@gmail.com>
2023-09-16 15:37:07 +0300

    work with lists

--- src/App.js
+++ src/App.js
@@ -3,25 +3,33 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import "./styles/App.css";
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
-  const [value, setValue] = useState("Text in Input");
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript 1", body: "Description" },
+    { id: 2, title: "JavaScript 2", body: "Description" },
+    { id: 3, title: "JavaScript 3", body: "Description" },
+  ]);
+
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: "Python 1", body: "Description" },
+    { id: 2, title: "Python 2", body: "Description" },
+    { id: 3, title: "Python 3", body: "Description" },
+  ]);
 
   return (
     <div className="App">
       {/* <Counter /> */}
       {/* <ClassCounter /> */}
-      {/* <div className="post">
-        <div className="post__content">
-          <strong>1. JavaScript</strong>
-          <div>JS is a programming language</div>
-        </div>
-        <div className="post__btns">
-          <button>Remove</button>
-        </div>
-      </div> */}
-      {/* <PostItem value={123} item={{ title: 0 }} str={"string"} /> */}
-      <PostItem post={{ id: 1, title: "JavaScript", body: "Description" }} />
+      {/* <PostItem post={{ id: 1, title: "JavaScript", body: "Description" }} /> */}
+      {/* <h1 style={{ textAlign: "center" }}>List of posts</h1>
+      {posts.map((post) => (
+        // <div>post</div>
+        <PostItem post={post} />
+      ))} */}
+      <PostList posts={posts} title="List of JS posts" />
+      <PostList posts={posts2} title="List of Python posts" />
     </div>
   );
 }
