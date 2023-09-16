ce766733990482740fce487670f4913e8bcae4c8
rinat <mambetovpost@gmail.com>
2023-09-16 14:45:15 +0300

    arguments of component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,29 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import "./styles/App.css";
+import PostItem from "./components/PostItem";
 
 function App() {
-  // 1
-  // let likes = 0;
-  // function increment() {
-  //   likes++;
-  //   console.log("likes", likes);
-  // }
-
-  // 2
-  // const state = useState(5);
-
-  // 3
-  // const [count, setCount] = useState(5);
-  // console.log("count", count);
-  // console.log("setCount", setCount);
-
-  // 4
-  // const [likes, setLikes] = useState(0);
-
-  // function increment() {
-  //   setLikes(likes + 1);
-  // }
-
-  // function decrement() {
-  //   setLikes(likes - 1);
-  // }
-
   const [value, setValue] = useState("Text in Input");
 
   return (
     <div className="App">
-      <Counter />
-      <ClassCounter />
+      {/* <Counter /> */}
+      {/* <ClassCounter /> */}
+      {/* <div className="post">
+        <div className="post__content">
+          <strong>1. JavaScript</strong>
+          <div>JS is a programming language</div>
+        </div>
+        <div className="post__btns">
+          <button>Remove</button>
+        </div>
+      </div> */}
+      {/* <PostItem value={123} item={{ title: 0 }} str={"string"} /> */}
+      <PostItem post={{ id: 1, title: "JavaScript", body: "Description" }} />
     </div>
   );
 }
 
 export default App;
-// hello
--- src/index.js
+++ src/index.js
@@ -4,18 +4,3 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-// root.render(
-// 1
-// <div>
-//   <p>hello</p>
-//   <button>Btn</button>
-// </div>
-// 2
-// React.createElement("button", { disabled: true }, "Press me!")
-// 3
-// React.createElement(
-//   "button",
-//   { onClick: () => console.log("Count") },
-//   "Press me!"
-// )
-// );
